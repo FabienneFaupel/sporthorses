8f17f3261bd29e566ecb6e1336f593de6e863c1f
Fabienne Faupel <l.faupel@maximator-hydrogen.de>
2025-12-19 10:56:58 +0100

    fehler in feed condume und add behonben

--- src/app/components/feed-consume-dialog/feed-consume-dialog.component.ts
+++ src/app/components/feed-consume-dialog/feed-consume-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -30,6 +31,7 @@ export interface ConsumeDialogData {
     MatNativeDateModule,
     MatSelectModule,
     MatCardModule,
+    CommonModule,
 
   ],
   templateUrl: './feed-consume-dialog.component.html',
