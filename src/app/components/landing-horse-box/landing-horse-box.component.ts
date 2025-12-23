59044cb29ff29f0aad9f74a07813ff526c289142
Fabienne Faupel <l.faupel@maximator-hydrogen.de>
2025-12-23 23:59:35 +0100

    start horse box angepasst

--- src/app/components/landing-horse-box/landing-horse-box.component.ts
+++ src/app/components/landing-horse-box/landing-horse-box.component.ts
@@ -1,8 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing-horse-box',
-  imports: [],
+  imports: [
+   CommonModule
+  ],
   templateUrl: './landing-horse-box.component.html',
   styleUrl: './landing-horse-box.component.scss'
 })
@@ -12,6 +15,16 @@ export class LandingHorseBoxComponent {
   @Input() horseAge!: number;
   @Input() horseBirth!: string;
   @Input() horseBreed!: string;
+  @Input() horseGender!: string;
+  
+
+  formatBirth(v?: string): string {
+  if (!v) return '';
+  const [y, m, d] = v.split('-');
+  if (!y || !m || !d) return v;
+  return `${d}.${m}.${y}`;
+}
+
 
 
 
