58ad005c0a2623c45d957db9dbbcd50ca830170d
Fabienne Faupel <l.faupel@maximator-hydrogen.de>
2025-12-26 19:25:17 +0100

    kleine feinheiten

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { ApplicationRef } from '@angular/core';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { concat, interval, first } from 'rxjs';
 
 
 
@@ -18,4 +21,20 @@ import { LandingPageComponent } from './pages/landing-page/landing-page.componen
 })
 export class AppComponent {
   title = 'SportHorses';
+
+constructor(appRef: ApplicationRef, updates: SwUpdate) {
+    if (!updates.isEnabled) return;
+
+    // regelmäßig nach Updates suchen (nachdem App stabil ist)
+    const appIsStable$ = appRef.isStable.pipe(first(isStable => isStable === true));
+    const every30s$ = interval(30_000);
+    concat(appIsStable$, every30s$).subscribe(() => updates.checkForUpdate());
+
+    // nur wenn wirklich eine neue Version bereit ist -> aktivieren + reload
+    updates.versionUpdates.subscribe(evt => {
+      if (evt.type === 'VERSION_READY') {
+        updates.activateUpdate().then(() => document.location.reload());
+      }
+    });
+  }
 }
